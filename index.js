99955da089f98ea653850def349cdf1740e9b182
Matt Hough <matt@hough.tv>
2018-01-04 07:46:33 +1000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much."
+
+function fatBastard(meal) {
+  function whatsForDinner() {   // closure function
+    if (meal === "Mini-me") {
+      console.log("The wee man is in my belly!");
+    } else {
+      console.log(`I'm eatin' a bit of ${meal}! Burp.`);
+    }
+  }
+  return whatsForDinner;
+}
